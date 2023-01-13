1bcde7f2f7071282bf57572b2a39f3d888ee641f
Rajat Kumar <rajatrana.rk@gmail.com>
2023-01-13 12:44:50 +0530

    delete

--- src/model/Discount.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const discountSchema = new mongoose.Schema({
-book_id: {type: String, required:true},
-discount_percentage: {type: Number, required:true}
-})
-
-const Discount = mongoose.model('Discount', discountSchema);
-module.exports = Discount;
